10207ce3a0bc01fa8b264efad78ae3e3ac9b4565
andreasasso9 <andry-sasso@outlook.it>
2023-07-17 18:23:33 +0200

    aggiunto javascript a signup.jsp per controllo campi form

--- Progetto/webapp/common/signup.jsp
+++ Progetto/webapp/common/signup.jsp
@@ -4,6 +4,7 @@
 <meta charset="ISO-8859-1">
 <title>Sign up</title>
 <link rel="stylesheet" href="<%=request.getContextPath() %>/styles/login.css">
+<script src="<%=request.getContextPath() %>/scripts/validate.js"></script>
 </head>
 <body>
 	<div id="container">
@@ -11,19 +12,28 @@
 			<fieldset>
 			<legend>Registrati</legend>
 				<form action="Signup" method="post">
-					<input type="text" name="nome" placeholder="Nome" id="nome"><br>
+					<input type="text" name="nome" placeholder="Nome"
+					onchange="validateFormElem(this, nameOrLastnamePattern, document.getElementById('errorName'), nameErrorMessage)"><span id="errorName"></span><br>
 					
-					<input type="text" name="cognome" placeholder="Cognome" id="cognome"><br>
+					<input type="text" name="cognome" placeholder="Cognome"
+					onchange="validateFormElem(this, nameOrLastnamePattern, document.getElementById('errorLastname'), nameErrorMessage)"><span id="errorLastname"></span><br>
 					
-					<input type="number" name="età" placeholder="età" id="età"><br>
+					<input type="text" name="età" placeholder="Età"
+					onchange="validateFormElem(this, etaPattern, document.getElementById('errorEtà'), etaErrorMessage)"><span id="errorEtà"></span><br>
 					
-					<input type="tel" name="telefono" placeholder="Telefono" id="telefono"><br>
+					<div id="phones">
+						<input type="text" name="telefono" placeholder="Telefono"  class="dati" id="telefono0"
+						onchange="validateFormElem(this, phonePattern, document.getElementById('errorPhone0'), phoneErrorMessage)">
+						<input type="button" value="+" onclick="addPhone()"><span id="errorPhone0"></span>
+					</div>
 					
-					<input type="text" name="username" placeholder="username" id="username"><br>
+					<input type="text" name="username" placeholder="Username"><br>
 					
-					<input type="email" name="email" placeholder="E-mail" id="email"><br>
+					<input type="text" name="email" placeholder="E-mail"
+					onchange="validateFormElem(this, emailPattern, document.getElementById('errorEmail'), emailErrorMessage)"><span id="errorEmail"></span><br>
 					
-					<input type="password" name="password" placeholder="password" id="password"><br>
+					<input type="password" name="password" placeholder="password"
+					onchange="validateFormElem(this, passPattern, document.getElementById('errorPassword'), passErrorMessage)"><span id="errorPassword"></span><br>
 					
 					<input type="submit" id="submit">
 					<input type="reset" id="reset">
---
+++ Progetto/webapp/scripts/validate.js
@@ -0,0 +1,98 @@
+const nameOrLastnamePattern = /^[A-Za-z]+$/;
+const emailPattern = /^\S+@\S+\.\S+$/;
+const phonePattern = /^([0-9]{3}-[0-9]{7})$/;
+const etaPattern = /^\d+$/;
+const passPattern = /^.{8,}$/;
+const nameErrorMessage = "<br>Inserisci solo lettere";
+const lastnameErrorMessage = "<br>Inserisci solo lettere";
+const emailErrorMessage = "<br>Una email valid deve essere del tipo username@domain.ext";
+const phoneErrorMessage = "<br>Un numero valido deve contenere solo numeri";
+const etaErrorMessage = "<br>Inserisci solo numeri";
+const passErrorMessage = "<br>La password deve essere lunga almeno 8 caratteri"
+let count = 1;
+
+function validate() {
+	let valid = true;	
+	let form = document.getElementById("form");
+	
+	let spanName = document.getElementById("errorName");
+	if(!validateFormElem(form.nome, nameOrLastnamePattern, spanName, nameErrorMessage)){
+		valid = false;
+	} 
+	let spanLastname = document.getElementById("errorLastname");
+	if (!validateFormElem(form.cognome, nameOrLastnamePattern, spanLastname, lastnameErrorMessage)){
+		valid = false;
+	}
+	let spanEmail = document.getElementById("errorEmail");
+	if (!validateFormElem(form.email, emailPattern, spanEmail, emailErrorMessage)){
+		valid = false;
+	}
+	
+	for (let i = 0; i < count; i++){
+		let spanPhone = document.getElementById("errorPhone" + i);
+		if (spanPhone == null){ // It has been removed
+			continue;
+		} else {
+			if (!validateFormElem(document.getElementById("telefono" + i), phonePattern, spanPhone, phoneErrorMessage)){
+				valid = false;
+			}
+		}	
+	}
+	
+	let spanEta=document.getElementById("errorEtà");
+	if (!validateFormElem(form.età, etaPattern, spanEta, etaErrorMessage)){
+		valid=false;
+	}
+	
+	let spanPassword=document.getElementById("errorPassword");
+	if (!validateFormElem(form.password, passPattern, spanPassword, passErrorMessage));
+	
+	return valid;
+}
+
+function validateFormElem(formElem, pattern, span, message) {
+	if(formElem.value.match(pattern)){
+		formElem.classList.remove("error");
+		span.style.color = "black";
+		span.innerHTML = "";
+		return true;
+	}
+	formElem.classList.add("error");
+	span.innerHTML = message;
+	span.style.color = "white";
+	return false;
+}
+
+function addPhone() {
+	let container = document.getElementById("phones");
+	
+	let div = document.createElement("div");
+	div.id = "phoneRow" + count;
+	
+	let element = document.createElement("input");
+	element.type = "text";
+	element.name = "telefono";
+	element.id = "phone" + count;
+	element.placeholder = "Telefono";
+	div.appendChild(element);
+	
+	let input = document.createElement("input");
+	input.type = "button";
+	input.value = "-";
+	input.addEventListener("click", function() {removePhone(div.id)});
+	div.appendChild(input);
+	
+	let span = document.createElement("span");
+	span.id = "errorPhone" + count;
+	div.appendChild(span);
+	
+	count++;
+	
+	container.appendChild(div);
+	
+}
+
+function removePhone(id) {
+	let element = document.getElementById(id);
+	element.parentNode.removeChild(element);
+}
